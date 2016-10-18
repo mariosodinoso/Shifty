8e47046901d5eb7a778f7e06d56f2a67dac09bfa
Mario Dietze <MariosoDinoso@dietzeonline.de>
2016-10-18 16:31:32 +0200

    First Skeleton

---
+++ Shifty/app/src/main/java/hsesslingen_aim1/shifty/EmployeeView.java
@@ -0,0 +1,13 @@
+package hsesslingen_aim1.shifty;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EmployeeView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_employee_view);
+    }
+}
